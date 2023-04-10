8702d256ac7dfe8195b4277631f575411e12fe56
Zmeegolov <karantinz@mail.ru>
2023-04-10 13:49:11 +0300

    Добавьте файлы проекта.

---
+++ junior11/Program.cs
@@ -0,0 +1,11 @@
+﻿Random rand = new Random();
+int number = rand.Next(101);
+int sum = 0;
+
+for (int i = 0; i <= number; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0) sum += i;
+}
+
+Console.WriteLine($"Число {number}");
+Console.WriteLine($"Сумма положительных чисел меньше {number}, кратных 3 или 5 {sum}");
